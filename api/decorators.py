3cd6b477b04fd3d8d1b410f8b46ada4821cd31f3
Odrian <adrian46tver@bk.ru>
2021-06-10 21:15:13 +0300

    _

--- api/decorators.py
+++ api/decorators.py
@@ -6,13 +6,13 @@ from .models import Session
 
 def session_check(func):
     def wrapper(*args, **kwargs):
-        '''key = args[0].POST.get('session')
+        key = args[0].POST.get('session')
         session = Session.objects.filter(key=key)
         if len(session) == 0:
             return JsonResponse({'info':'session_error', 'path':'auth.phone'})
         session = session[0]
         session.last_activity = now()
         session.save()
-        return func(*args, phone=session.phone, **kwargs)'''
+        return func(*args, phone=session.phone, **kwargs)
         return func(*args, phone='', **kwargs)
     return wrapper
