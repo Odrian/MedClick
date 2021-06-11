35ab3fedc46488209a3935fa61c6aa3c0ecbd140
gamer <adrian46tver@bk.ru>
2021-06-11 12:42:18 +0300

    _

--- MedClick/settings.py
+++ MedClick/settings.py
@@ -113,6 +113,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'index/static'),
     os.path.join(BASE_DIR, 'user/static'),
+    os.path.join(BASE_DIR, 'chat/static'),
 ]
 
 # Default primary key field type
